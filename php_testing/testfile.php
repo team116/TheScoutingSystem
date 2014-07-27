8b0d2243355e95e6f67241c82fa6f5ddce030b1c
TeleTurbans <attars83@gmail.com>
2014-07-27 16:40:10 -0400

    MADE IT NICE

--- php_testing/testfile.php
+++ php_testing/testfile.php
@@ -8,9 +8,9 @@
     <body>s
         <p>
             <?php
-session_start();
-echo "Your session ID is ".session_id();
-setcookie("ID","PASSWORD" ,time() +3600, "/" ,"localhost"); 
+            session_start();
+            echo "Your session ID is ".session_id();
+            setcookie("ID","PASSWORD" ,time() +3600, "/" ,"localhost"); 
             // this line will get rid of error warnings, only use when this file is completely tested
             error_reporting(E_ERROR);
             if (isset($_POST['textbox'])) {
