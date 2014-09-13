0d491afda734a57dd2c59c38480bf64c3ecebb41
watermelonduck <lordofthemaracas@gmail.com>
2014-09-13 15:24:47 -0400

    figuring out session stuff

---
+++ php_testing/ReadSession.php
@@ -0,0 +1,9 @@
+<?php
+error_reporting(-1); // display all faires
+ini_set('display_errors', 1);  // ensure that faires will be seen
+ini_set('display_startup_errors', 1); // display faires that didn't born
+
+session_start();
+
+echo $_SESSION["message"];
+?>
